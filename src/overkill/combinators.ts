03f1d61d9c69064c9243894b66f346bd7cf47428
Herrington Darkholme <nonamesheep1@gmail.com>
2015-08-31 01:16:58 +0800

    add experimental observable combinators

---
+++ src/overkill/combinators.ts
@@ -0,0 +1,19 @@
+import {Rx, Var, getSinal} from './index'
+import {peek as _peek} from './ok'
+
+type _ = {}
+
+function map<U, V>(v: Var<U>, fn: (t:U) => V): Rx<V, _> {
+  return Rx(() => fn(v()))
+}
+
+function filter<U>(v: Var<U>, fn: (t: U) => boolean): Rx<U, _> {
+  return Rx(() => {
+    if (fn(v())) return v()
+  })
+}
+
+function peek<U>(o: Var<U>): U {
+  let sig: any = getSinal(o)
+  return <any>_peek(sig)
+}
